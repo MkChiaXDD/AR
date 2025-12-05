c0a45b85cad0d31898ca60844c62e5c2f7da5414
MkChiaXDD <mengkiatchia@gmail.com>
2025-12-05 11:58:42 +0800

    a

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,8 @@ public class GameManager : MonoBehaviour
 
     public Transform DefenseBase { get; private set; }
 
+    public GameObject BalloonPrefab;
+
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -25,10 +27,17 @@ public class GameManager : MonoBehaviour
         DefenseBase = baseTransform;
         // Here you can also trigger: enable UI, allow waves, etc.
         Debug.Log("Defense base set: " + baseTransform.name);
+        StartGame();
     }
 
     public bool HasDefenseBase()
     {
         return DefenseBase != null;
+    }
+
+    private void StartGame()
+    {
+        var newBalloon = Instantiate(BalloonPrefab);
+        newBalloon.GetComponent<Balloon>().Init(DefenseBase);
     }
 }
